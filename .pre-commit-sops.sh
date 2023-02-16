adc713c92f994faf12cdaca544c404b1b29da704
QJoly <quentinjoly@thoughtless.eu>
2023-02-16 17:07:53 +0100

    Trooo

--- .pre-commit-sops.sh
+++ .pre-commit-sops.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-filename="$1"
 log_file="./pre-commit.log"
-echo $@ | tee $log_file
+for filename in "$@"
+do
+    echo "Checking.... $filename"
 if [[ "$filename" =~ ^secret\..*\.ya?ml$ ]]; then
-  echo "$filename matches pattern, encrypting..." | tee $log_file
+  echo "$filename matches pattern, encrypting..." >> $log_file
   sops -e -i $filename
   git add $filename
 else
-  echo "$filename does not match pattern and will not be encrypted" | tee $log_file
+  echo "$filename does not match pattern and will not be encrypted" >> $log_file
 fi
+done
+
 
