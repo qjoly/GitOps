2287c3edb5bc360f4bc218db34dc1b86b55d2bb8
QJoly <quentinjoly@thoughtless.eu>
2023-03-07 15:01:09 +0100

    Création tache Terraform

---
+++ VM/Libvirt/variables.tf
@@ -0,0 +1,29 @@
+﻿variable "provisionning_debian_memory" {
+    type        = number
+    default     = 1024
+    description = "This is another example input variable using env variables."
+}
+
+variable "provisionning_debian_cpu" {
+    type        = number
+    default     = 1
+    description = "This is another example input variable using env variables."
+}
+
+variable "source_file" {
+    type        = string
+    default     = "../../artifacts/packer-Debian"
+    description = "This is another example input variable using env variables."
+}
+
+variable "hypervisor_libvirt_pool_dir" {
+    type        = string
+    description = "This is another example input variable using env variables."
+}
+
+variable "hypervisor_libvirt_pool_name" {
+    type        = string
+    default     = "clusterOps"
+    description = "This is another example input variable using env variables."
+}
+
