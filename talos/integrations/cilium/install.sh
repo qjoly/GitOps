4cb759e264721c04796f4eb9c95a0728ad865cc9
QJoly <github@une-tasse-de.cafe>
2023-12-12 13:05:02 +0100

    create a dir for cilium + prepare for L2 announcement

---
+++ talos/integrations/cilium/install.sh
@@ -0,0 +1,15 @@
+cilium install \
+    --helm-set=ipam.mode=kubernetes \
+    --helm-set=kubeProxyReplacement=true \
+    --helm-set=securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --helm-set=securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --helm-set=cgroup.autoMount.enabled=false \
+    --helm-set=cgroup.hostRoot=/sys/fs/cgroup \
+    --helm-set=k8sServiceHost=localhost \
+    --helm-set=k8sServicePort=7445 \
+    --helm-set=hubble.relay.enabled=true \
+    --helm-set=hubble.ui.enabled=true \
+    --helm-set=externalIPs.enabled= true \
+    --helm-set=l2announcements.enabled=true \
+    --helm-set=k8sClientRateLimit.qps=50 \
+    --helm-set=k8sClientRateLimit.burst=100
