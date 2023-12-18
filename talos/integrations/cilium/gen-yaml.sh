85d3aa48d1de9601b64579b20db1d579e664272b
QJoly <github@une-tasse-de.cafe>
2023-12-18 22:31:36 +0100

    Generate a yaml based on Helm install method

---
+++ talos/integrations/cilium/gen-yaml.sh
@@ -0,0 +1,25 @@
+helm template \
+    cilium \
+    cilium/cilium \
+    --version 1.14.0 \
+    --namespace kube-system \
+    --set ipam.mode=kubernetes \
+    --set=kubeProxyReplacement=true \
+    --set=securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set=securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set=cgroup.autoMount.enabled=false \
+    --set=cgroup.hostRoot=/sys/fs/cgroup \
+    --set=k8sServiceHost=localhost \
+    --set=k8sServicePort=7445 \
+    --set=l2announcements.enabled=true \
+    --set=l2announcements.leaseDuration="300s" \
+    --set=l2announcements.leaseRenewDeadline="60s" \
+    --set=l2announcements.leaseRetryPeriod="10s" \
+    --set=externalIPs.enabled=true \
+    --set=hubble.relay.enabled=true \
+    --set=hubble.ui.enabled=true
+    
+#    --helm-set=k8sClientRateLimit.qps=50 \
+#    --helm-set=k8sServiceHost=localhost \
+#    --helm-set=k8sServicePort=7445 \
+#    --helm-set=k8sClientRateLimit.burst=100
